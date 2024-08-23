484479426171c09c6ecc36e0c946289d7898cf86
mete0rfish <sungwon326@naver.com>
2024-08-23 10:26:06 +0900

    security: Tomca에서 https로 전달하도록 수정
     - Spring Boot가 X-Forwarded-* 헤더를 인식하도록 하며, 프록시나 로드 밸런서에서 전달되는 원래의 프로토콜을 기반으로 리다이렉트를 처리하게 합니다.

---
+++ dev-route/src/main/java/com/teamdevroute/devroute/global/config/TomcatConfig.java
@@ -0,0 +1,16 @@
+package com.teamdevroute.devroute.global.config;
+
+import org.apache.catalina.valves.RemoteIpValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+        return factory -> factory.addEngineValves(new RemoteIpValve());
+    }
+}
