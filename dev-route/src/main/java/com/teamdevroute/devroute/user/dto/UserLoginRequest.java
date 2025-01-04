50dd9a4a8fa76645f0085bb929566781eb2bcf81
mete0rfish <sungwon326@naver.com>
2025-01-04 11:47:31 +0900

    refactor: UserLoginRequest를 record로 변경

---
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/dto/UserLoginRequest.java
@@ -0,0 +1,19 @@
+package com.teamdevroute.devroute.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public record UserLoginRequest(
+        @NotNull(message = "이메일은 null 일 수 없습니다.")
+        @Email(message = "유효한 이메일 형식이어야 합니다.")
+        String email,
+
+        @NotNull(message = "비밀번호는 null 일 수 없습니다.")
+        String password
+) {
+
+}
