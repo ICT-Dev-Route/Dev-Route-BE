932e7342fff9fdcff91ecf48cca98903d6405898
mete0rfish <sungwon326@naver.com>
2025-01-04 11:40:49 +0900

    refactor: UserController, LoginType 수정
    - 불필요한 ResponseEntity 삭제
    - 각 외부 로그인을 하나의 매핑으로 처리하도록 수정
    - LoginType을 key-value 형식으로 사용

--- dev-route/src/main/java/com/teamdevroute/devroute/user/UserController.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/UserController.java
@@ -7,6 +7,7 @@ import com.teamdevroute.devroute.user.dto.UserCreateResponse;
 import com.teamdevroute.devroute.user.enums.LoginType;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -20,28 +21,24 @@ import java.net.URI;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
     @Value("${domain.url}")
     private String DOMAIN_URL;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @PostMapping("/signup")
-    public ResponseEntity createUser(@RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserCreateResponse> createUser(@RequestBody UserCreateRequest request) {
         UserCreateResponse response = userService.createUser(request);
         return ResponseEntity.created(URI.create("/users/" + response.id())).body(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(
-            @RequestBody UserLoginRequest loginRequest
-    ) throws IOException {
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest loginRequest) throws IOException {
         String token = userService.login(loginRequest);
-        log.info("토큰: " + token);
+        log.info("생성된 토큰: {}", token);
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + token);
@@ -49,55 +46,26 @@ public class UserController {
         return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
-    @GetMapping("/auth/kakao")
-    public ResponseEntity getKakaoRedirectUrl() {
-        String url = userService.getRedirectUrl(LoginType.KAKAO);
-        return new ResponseEntity<>(url, HttpStatus.OK);
+    @GetMapping("/auth/{provider}")
+    public ResponseEntity<String> getRedirectUrl(@PathVariable String provider) {
+        LoginType loginType = LoginType.toEnum(provider);
+        String url = userService.getRedirectUrl(loginType);
+        return ResponseEntity.ok(url);
     }
 
-    @GetMapping("/auth/google")
-    public ResponseEntity getGoogleRedirectUrl() {
-        String url = userService.getRedirectUrl(LoginType.GOOGLE);
-        return new ResponseEntity<>(url, HttpStatus.OK);
-    }
-
-    @GetMapping("/auth/naver")
-    public ResponseEntity getNaverRedirectUrl() {
-        String url = userService.getRedirectUrl(LoginType.NAVER);
-        return new ResponseEntity<>(url, HttpStatus.OK);
-    }
-
-    @GetMapping("/auth/kakao/callback")
-    public ResponseEntity<String> kakaoCallback(@RequestParam("code") String code, HttpSession session, HttpServletResponse response) throws IOException {
-        String accessToken = userService.getAccessToken(code, LoginType.KAKAO, null);
+    @GetMapping("/auth/{provider}}/callback")
+    public void handleCallback(
+            @PathVariable String provider,
+            @RequestParam("code") String code,
+            HttpSession session,
+            HttpServletResponse response
+    ) throws IOException {
+        LoginType loginType = LoginType.toEnum(provider);
+        String accessToken = userService.getAccessToken(code, loginType, null);
         String token = userService.authLogin(accessToken, LoginType.KAKAO);
 
-        session.setAttribute("token" ,"Bearer " + token);
+        session.setAttribute("token", "Bearer " + token);
         response.sendRedirect(DOMAIN_URL + "/mainpage");
-
-        return new ResponseEntity<>("유저가 로그인되었습니다.", HttpStatus.OK);
-    }
-
-
-    @GetMapping("/auth/google/callback")
-    public ResponseEntity<String> googleCallback(@RequestParam("code") String code, HttpSession session, HttpServletResponse response) throws IOException {
-        String accessToken = userService.getAccessToken(code, LoginType.GOOGLE, null);
-        String token = userService.authLogin(accessToken, LoginType.GOOGLE);
-
-        session.setAttribute("token" ,"Bearer " + token);
-        response.sendRedirect(DOMAIN_URL + "/mainpage");
-
-        return new ResponseEntity<>("유저가 로그인되었습니다.", HttpStatus.OK);
-    }
-
-    @GetMapping("/auth/naver/callback")
-    public ResponseEntity<String> naverCallback(@RequestParam("code") String code, @RequestParam("state") String state, HttpSession session, HttpServletResponse response) throws IOException {
-        String accessToken = userService.getAccessToken(code, LoginType.NAVER, state);
-        String token = userService.authLogin(accessToken, LoginType.NAVER);
-
-        session.setAttribute("token" ,"Bearer " + token);
-        response.sendRedirect(DOMAIN_URL + "/mainpage");
-        return new ResponseEntity<>("유저가 로그인되었습니다.", HttpStatus.OK);
     }
 
     @GetMapping("/token")
@@ -106,25 +74,25 @@ public class UserController {
         if (token != null) {
             return ResponseEntity.ok(token);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("No token found");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰을 찾을 수 없습니다.");
         }
     }
 
     @GetMapping("/login/test")
     public ResponseEntity<String> loginTest(@AuthenticationPrincipal CustomUserDetails userDetails) throws IOException {
         LoginUserInfo user = userDetails.user();
-        System.out.println("user = " + user.toString());
-        return new ResponseEntity<>("유저가 로그인되었습니다.", HttpStatus.OK);
+        log.info("유저 정보: {}", user.toString());
+        return ResponseEntity.ok("유저가 로그인되었습니다.");
     }
 
     @GetMapping("/mypage")
-    public ResponseEntity readMyPage(@AuthenticationPrincipal CustomUserDetails userDetails){
+    public ResponseEntity<?> readMyPage(@AuthenticationPrincipal CustomUserDetails userDetails) {
         LoginUserInfo user = userDetails.user();
         return ResponseEntity.ok(userService.readMyPage(user.getId()));
     }
 
     @GetMapping("/healthcheck")
-    public String healthcheck(){
+    public String healthcheck() {
         return "OK";
     }
 }
--- dev-route/src/main/java/com/teamdevroute/devroute/user/enums/LoginType.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/enums/LoginType.java
@@ -1,5 +1,20 @@
 package com.teamdevroute.devroute.user.enums;
 
+
+import java.util.Locale;
+
 public enum LoginType {
-    KAKAO, NAVER, GOOGLE, NORMAL
+    KAKAO,
+    NAVER,
+    GOOGLE,
+    NORMAL;
+
+    public static LoginType toEnum(String name) {
+        return switch (name.toLowerCase()) {
+            case "google" -> GOOGLE;
+            case "kakao" -> KAKAO;
+            case "naver" -> NAVER;
+            default -> NORMAL;
+        };
+    }
 }
