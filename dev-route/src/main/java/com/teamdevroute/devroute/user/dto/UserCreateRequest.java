6e6dc2bd7d96c5792964a0d139d3c84bfb47d1e9
mete0rfish <sungwon326@naver.com>
2025-01-04 10:16:48 +0900

    refactor: record DTO의 @Builder 위치 수정

--- dev-route/src/main/java/com/teamdevroute/devroute/user/dto/UserCreateRequest.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/dto/UserCreateRequest.java
@@ -5,6 +5,7 @@ import com.teamdevroute.devroute.user.enums.DevelopField;
 import lombok.Builder;
 import lombok.Setter;
 
+@Builder
 public record UserCreateRequest (
        String email,
        String password,
@@ -12,15 +13,6 @@ public record UserCreateRequest (
        DevelopField development_field,
        String loginType
 ) {
-
-    @Builder
-    public UserCreateRequest(String email, String password, String name, DevelopField development_field, String loginType) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.development_field = development_field;
-        this.loginType = loginType;
-    }
 
     public User toEntity(String loginType, String encoded){
         return User.builder()
